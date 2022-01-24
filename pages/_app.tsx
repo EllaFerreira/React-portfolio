98d1de606e9ab091fbecd9618acfab79b910261d
Ella <rafaela.morato1805@gmail.com>
2022-01-24 21:29:52 +1000

    hello message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '../styles/globals.css'
+// import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
 
-export default MyApp
+// export default MyApp
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(React.createElement("h2", null, "Hello"), document.body);
