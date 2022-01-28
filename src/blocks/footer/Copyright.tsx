4bf6efb0e1a3b78fd3b640542141a33d6b84e32b
Ella <rafaela.morato1805@gmail.com>
2022-01-28 21:44:53 +1000

    components and data

---
+++ src/blocks/footer/Copyright.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Copyright = () => {
+  return (
+    <div className="copyright">
+      <p>
+        © 2022 Portfolio Coded By{" "}
+        <a href="">Ella</a>
+      </p>
+    </div>
+  );
+};
+
+export default Copyright;
